4f14a4a5c99ca9058d883facf512bf2fd75d173a
Eoin McGrath <eoin@beacon.by>
2024-04-26 08:44:35 +0300

    adds npm run zip command

---
+++ zip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+VER=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
+COMMIT=$(git rev-parse --short=6 HEAD)
+
+FILE="powersurge-${COMMIT}.zip"
+
+
+rm *.zip
+zip -r $FILE dist/
+
+
+echo -e "\n-----------------\n"
+echo "CREATED: ${FILE}"
